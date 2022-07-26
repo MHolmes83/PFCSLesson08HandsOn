bb134d59961b7c4dcf4056a1837c96d5b0dbb1ff
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2022-07-26 12:41:34 -0400

    Lesson08HandsOn
    Created a new console application can created two interfaces called Mammal and Animal with methods set up for each of them. Then created three classes that inherited those interfaces called Dog, Cat, and Cow, then instantiated them through the Main() method in the Program class.

---
+++ Lesson08HandsOn/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Lesson08HandsOn
+{
+    public interface Mammal
+        {
+            string speak();
+            string run();
+        }
+
+    public interface Animal
+    {
+        string eat();
+    }
+
+    public class Dog : Mammal, Animal
+    {
+        private string name;
+
+        public Dog(string name)
+        {
+            this.name = name;
+        }
+
+        public string speak()
+        {
+            return "Bark!";
+        }
+        
+        public string run()
+        {
+            return "Dogs can run at a top speen of 45 mph!";
+        }
+
+        public string eat()
+        {
+            return "Dogs eat bones.";
+        }
+    }
+
+    public class Cat : Mammal, Animal
+    {
+        private string name;
+
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+
+        public string speak()
+        {
+            return "Meow!";
+        }
+
+        public string run()
+        {
+            return "Cats can run at a top speed of 30 mph!";
+        }
+
+        public string eat()
+        {
+            return "Cats eat mice.";
+        }
+    }
+
+    public class Cow : Mammal, Animal
+    {
+        private string name;
+
+        public Cow(string name)
+        {
+            this.name = name;
+        }
+
+        public string speak()
+        {
+            return "Moo!";
+        }
+
+        public string run()
+        {
+            return "Cows can run at a top speed of 25 mph";
+        }
+
+        public string eat()
+        {
+            return "Cows eat grass.";
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Dog spot = new Dog("Spot");
+            Console.WriteLine(spot.speak());
+            Console.WriteLine(spot.run());
+            Console.WriteLine(spot.eat());
+
+            Cat fluffy = new Cat("Fluffy");
+            Console.WriteLine(fluffy.speak());
+            Console.WriteLine(fluffy.run());
+            Console.WriteLine(fluffy.eat());
+
+            Cow bessie = new Cow("Bessie");
+            Console.WriteLine(bessie.speak());
+            Console.WriteLine(bessie.run());
+            Console.WriteLine(bessie.eat());
+        }
+    }
+}
